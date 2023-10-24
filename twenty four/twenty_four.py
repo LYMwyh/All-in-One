cd295bf90d48ea0ddb89aaaf058896110da7b210
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-10-24 21:45:05 +0800

    Update twenty_four.py

--- twenty four/twenty_four.py
+++ twenty four/twenty_four.py
@@ -17,8 +17,10 @@ print_text = Text(print_frame_border, bg="black", fg="white")
 
 input_frame_border = Frame(root, bg="white", bd=1)
 input_frame = Frame(input_frame_border, bg="black")
+example = Text(input_frame, bg="black", fg="white")
 input_text = Text(input_frame, bg="black", fg="white")
 
+button_next = Button(input_frame, text="Next")
 button_yes = Button(input_frame, text="YES")
 button_no = Button(input_frame, text="NO")
 button_close = Button(input_frame, text="closed", command=root.quit)
@@ -28,7 +30,9 @@ setting_content = [setting,
                    print_text,
                    button_yes,
                    button_no,
+                   example,
                    input_text,
+                   button_next,
                    button_close]
 
 setting_content_detail = {
@@ -37,8 +41,10 @@ setting_content_detail = {
 	2: [print_text, "Print Text"],
 	3: [button_yes, "Button Yes"],
 	4: [button_no, "Button No"],
-	5: [input_text, "Input text"],
-	6: [button_close, "Button Close"]
+	5: [example, "Example"],
+	6: [input_text, "Input text"],
+	7: [button_next, "Button Next"],
+	8: [button_close, "Button Close"]
 }
 
 fonts = tkinter.font.families()
@@ -111,11 +117,15 @@ def open_setting_window():
 	setting_entry = {}
 	setting_entry_button = {}
 	for key, detail in setting_content_detail.items():
-		setting_entry[detail[0]] = Entry(setting_frame[detail[0]], validate="key", validatecommand=(setting_window.register(only_int_input), '%P'))
+		setting_entry[detail[0]] = Entry(setting_frame[detail[0]], validate="key",
+		                                 validatecommand=(setting_window.register(only_int_input), '%P'))
 		setting_entry[detail[0]].insert('end', str(font_family[detail[0]]['size']))
-		setting_entry[detail[0]].bind('<FocusOut>', lambda event, entry=setting_entry[detail[0]], widget=detail[0]: on_focus_out(entry, widget))
+		setting_entry[detail[0]].bind('<FocusOut>',
+		                              lambda event, entry=setting_entry[detail[0]], widget=detail[0]: on_focus_out(
+			                              entry, widget))
 		setting_entry_button[detail[0]] = Button(setting_frame[detail[0]], text="Confirm")
-		setting_entry_button[detail[0]].config(command=lambda widget=detail[0], new_size=True: update_font_print(widget, font_size=new_size))
+		setting_entry_button[detail[0]].config(
+			command=lambda widget=detail[0], new_size=True: update_font_print(widget, font_size=new_size))
 	
 	setting_widget_hint = {}
 	for key, element in setting_content_detail.items():
@@ -141,7 +151,7 @@ def open_setting_window():
 			font_family['setting_window']['size'] -= 1
 		size_add.configure(font=font_family['setting_window'])
 		size_subtract.configure(font=font_family['setting_window'])
-		for ordinal_number in range(7):
+		for ordinal_number in range(9):
 			if ordinal_number == 1:
 				continue
 			setting_widget_hint[setting_content_detail[ordinal_number][0]][0].config(font=font_family['setting_window'])
@@ -154,7 +164,7 @@ def open_setting_window():
 		                                               variable=font_style_vars[1],
 		                                               command=lambda new_font=each_font: update_font_in_setting_window(
 			                                               update_font_style=new_font))
-		
+	
 	setting_menu_button['setting_window'].pack(fill='x', side='top', expand=True)
 	
 	size_add = Button(setting_frame['setting_window'], text="font size +",
@@ -173,13 +183,13 @@ def open_setting_window():
 		canvas.create_line(0, 0, canvas.winfo_width(), 0, width=canvas.winfo_height(), fill='black')
 	
 	dividing_line = []
-	for ordinal_number in range(8):
+	for ordinal_number in range(10):
 		if ordinal_number == 1:
 			continue
 		dividing_line.append(Canvas(setting_window, height=1))
 		dividing_line[-1].pack(fill='x')
 		dividing_line[-1].bind("<Configure>", lambda event, canvas=dividing_line[-1]: draw_line(canvas))
-		if ordinal_number == 7:
+		if ordinal_number == 9:
 			break
 		setting_widget_hint[setting_content_detail[ordinal_number][0]][0].grid(row=0, column=0)
 		setting_widget_hint[setting_content_detail[ordinal_number][0]][1].grid(row=1, column=0)
@@ -197,19 +207,27 @@ setting.configure(command=lambda: open_setting_window())
 
 def printer(content, text=print_text):
 	for char in content:
+		text.configure(state=NORMAL)
 		text.insert('end', char)
+		text.configure(state=DISABLED)
 		root.update()
 		time.sleep(0)
+	text.configure(state=NORMAL)
 	text.insert('end', '\n')
+	text.configure(state=DISABLED)
 
 
 def whether_know_whole_answer():
+	example.place_forget()
 	input_text.place_forget()
-	button_yes.place_forget()
-	button_yes.configure(text="YES")
+	button_next.place_forget()
+	
 	print_text.configure(state=NORMAL)
-	printer("Do you want to know the whole answer(s)?")
+	print_text.delete('1.0', 'end')
 	print_text.configure(state=DISABLED)
+	
+	printer("Do you want to know the whole answer(s)?")
+	
 	button_yes.configure(command=lambda: whole_answers(True))
 	button_no.configure(command=lambda: whole_answers(False))
 	button_yes.place(relx=0, rely=0, relwidth=1, relheight=0.5)
@@ -221,7 +239,8 @@ def submit_answers():
 	input_text.configure(state=DISABLED)
 	print_text.configure(state=NORMAL)
 	print_text.delete('1.0', 'end')
-	answers = input_text.get('2.0', 'end')
+	print_text.configure(state=DISABLED)
+	answers = input_text.get('1.0', 'end')
 	answers = answers.split('\n')
 	answers.pop()
 	# print(Whole_Answers)
@@ -254,11 +273,10 @@ def submit_answers():
 			printer("True")
 		else:
 			printer("False")
-	print_text.configure(state=DISABLED)
+	
 	submit.place_forget()
-	button_yes.configure(text="Next")
-	button_yes.configure(command=lambda: whether_know_whole_answer())
-	button_yes.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
+	button_next.configure(command=lambda: whether_know_whole_answer())
+	button_next.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
 
 
 submit = Button(input_frame, text="Submit", command=lambda: submit_answers())
@@ -268,10 +286,11 @@ def check_answer(yes):
 	button_yes.place_forget()
 	button_no.place_forget()
 	
-	print_text.configure(state=NORMAL)
 	printer("OK!")
 	time.sleep(1.0)
+	print_text.configure(state=NORMAL)
 	print_text.delete('1.0', 'end')
+	print_text.configure(state=DISABLED)
 	if yes is True:
 		printer(
 			"Four numbers: %d , %d , %d , %d ." % (
@@ -279,14 +298,15 @@ def check_answer(yes):
 				algorithm.Four_Numbers[3]))
 		printer("Please input your answer(s) in the input frame.")
 		printer("Each line write one answer.You don't need to write '=24' in the end.")
-		print_text.configure(state=DISABLED)
 		
+		example.configure(state=DISABLED)
+		example.place(relx=0, rely=0, relwidth=1, relheight=0.1)
 		input_text.configure(state=NORMAL)
 		input_text.delete('1.0', 'end')
-		input_text.place(relx=0, rely=0, relwidth=1, relheight=0.9)
+		input_text.place(relx=0, rely=0.1, relwidth=1, relheight=0.8)
 		submit.configure(state=NORMAL)
 		submit.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
-		printer("Example: a+b+c+d", input_text)
+		printer("Example: a+b+c+d", example)
 	else:
 		whether_know_whole_answer()
 
@@ -295,11 +315,12 @@ def whole_answers(yes):
 	button_yes.place_forget()
 	button_no.place_forget()
 	
-	print_text.configure(state=NORMAL)
 	printer('\n')
 	printer("OK!")
 	time.sleep(1.0)
+	print_text.configure(state=NORMAL)
 	print_text.delete('1.0', 'end')
+	print_text.configure(state=DISABLED)
 	if yes is True:
 		if len(algorithm.Whole_Answers) == 0:
 			printer("There is no any answers!")
@@ -308,7 +329,6 @@ def whole_answers(yes):
 				printer(Each_Answer + "=24")
 	printer("Do you want to play it again?")
 	
-	print_text.configure(state=DISABLED)
 	button_yes.configure(command=lambda: clicked_yes())
 	button_no.configure(command=lambda: clicked_no())
 	button_yes.place(relx=0, rely=0, relwidth=1, relheight=0.5)
@@ -325,11 +345,12 @@ def clicked_yes():
 	for _ in range(4):
 		algorithm.Four_Numbers.append(float(random.randint(1, 13)))
 	
-	print_text.configure(state=NORMAL)
 	printer('\n')
 	printer('OK!')
 	time.sleep(0.2)
+	print_text.configure(state=NORMAL)
 	print_text.delete('1.0', 'end')
+	print_text.configure(state=DISABLED)
 	printer("Here is four numbers: %d , %d , %d , %d ." % (
 		algorithm.Four_Numbers[0], algorithm.Four_Numbers[1], algorithm.Four_Numbers[2], algorithm.Four_Numbers[3]))
 	time.sleep(1)
@@ -338,8 +359,6 @@ def clicked_yes():
 	
 	printer("Did you find the answer(s)?")
 	printer("If yes, do you want to check your answer(s)?")
-	
-	print_text.configure(state=DISABLED)
 	
 	button_yes.configure(command=lambda: check_answer(True))
 	button_no.configure(command=lambda: check_answer(False))
@@ -351,11 +370,9 @@ def clicked_no():
 	button_yes.place_forget()
 	button_no.place_forget()
 	
-	print_text.configure(state=NORMAL)
 	printer('\n')
 	printer("OK!")
 	printer("See you next time!")
-	print_text.configure(state=DISABLED)
 	
 	button_close.place(relx=0, rely=0, relwidth=1, relheight=1)
 
@@ -363,6 +380,7 @@ def clicked_no():
 def start():
 	Start.place_forget()
 	print_frame_border.place(relx=0, rely=0.1, relwidth=0.5, relheight=0.9)
+	print_text.configure(state=DISABLED)
 	print_text.place(relx=0, rely=0, relwidth=1, relheight=1)
 	input_frame_border.place(relx=0.5, rely=0.1, relwidth=0.5, relheight=0.9)
 	input_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
@@ -374,7 +392,6 @@ def start():
 	printer("\n")
 	
 	printer("Do you want to play it with me?")
-	print_text.configure(state=DISABLED)
 	button_yes.configure(command=lambda: clicked_yes())
 	button_no.configure(command=lambda: clicked_no())
 	button_yes.place(relx=0, rely=0, relwidth=1, relheight=0.5)
