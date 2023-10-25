60514b44090059c1ea178da5adfabb01b4c9eb79
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-10-25 23:33:49 +0800

    Update debug.py

--- twenty four/debug.py
+++ twenty four/debug.py
@@ -1,26 +1,9 @@
-import tkinter as tk
+from tkinter import *
 
-def onFrameConfigure(canvas):
-    '''Reset the scroll region to encompass the inner frame'''
-    canvas.configure(scrollregion=canvas.bbox("all"))
+root = Tk()
 
-root = tk.Tk()
-canvas = tk.Canvas(root)
-frame = tk.Frame(canvas)
-
-# 添加滚动条到canvas
-vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-canvas.configure(yscrollcommand=vsb.set)
-
-vsb.pack(side="right", fill="y")
-canvas.pack(side="left", fill="both", expand=True)
-
-# 添加frame到canvas
-canvas.create_window((0,0), window=frame, anchor="nw")
-
-for i in range(50):
-    tk.Label(frame, text="This is line %i" % i).pack()
-
-frame.bind("<Configure>", lambda event, Canvas=canvas: onFrameConfigure(Canvas))
+# 创建一个Text组件，设置其行数为5
+text = Text(root, height=5)
+text.pack()
 
 root.mainloop()
