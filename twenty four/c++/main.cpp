3a2d87df663be666ed3eff30bca9aac0a1f75a79
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-10-27 16:26:29 +0800

    Create main.cpp

---
+++ twenty four/c++/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Yimin Liu on 27/10/2023.
+//
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        srand(static_cast<unsigned int>(time(nullptr)));
+    }
+    return 0;
+}
