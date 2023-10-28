2e270c01d2a292bcdb29c9733889c95b6eab1893
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-10-28 10:11:34 +0800

    Update debug.py

--- twenty four/python tkinter/debug.py
+++ twenty four/python tkinter/debug.py
@@ -1,12 +1,1 @@
-from tkinter import *
-
-root = Tk()
-
-frame1 = Frame(root, bg='black')
-frame2 = Frame(root, bg='green')
-label = Label(frame1, text='1111')
-frame1.pack(side='left')
-frame2.pack(side='right')
-label.pack()
-
-root.mainloop()
+# None
