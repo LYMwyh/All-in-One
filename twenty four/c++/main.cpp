0f27258fdc77555af0f9b9844621df001bb3d425
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-10-31 14:54:03 +0800

    Update main.cpp

--- twenty four/c++/main.cpp
+++ twenty four/c++/main.cpp
@@ -1,13 +1,23 @@
-//
-// Created by Yimin Liu on 28/10/2023.
-//
-#include<iostream>
+#include <random>
+#include <iostream>
 using namespace std;
-int main()
-{
-    while(true)
-    {
-        
-    }
+struct Fraction{
+    int numerator;
+    int denominator;
+}Four_Numbers;
+
+int main() {
+    // Create a random device
+    random_device rd;
+
+    // Initialize Mersenne Twister pseudo-random number generator
+    mt19937 gen(rd());
+
+    // Uniform distribution between 1 and 100
+    uniform_int_distribution<> dis(1, 100);
+    // Generate and output a random integer
+    for(int fraction;)
+    std::cout << dis(gen) << '\n';
+
     return 0;
 }
