1c91de62bccac9fbfac7dddad0b04ad8f13ba91a
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-07 16:08:49 +0800

    Update game_stats.py

--- Alien Invasion/game_stats.py
+++ Alien Invasion/game_stats.py
@@ -4,6 +4,10 @@ class GameStats():
 		self.reset_stats()
 		self.game_active = False
 		self.score = 0
+		self.level = 1
+		self.high_score = 0
 	
 	def reset_stats(self):
 		self.ships_left = self.ai_settings.ship_limit
+		self.score = 0
+		self.level = 1
