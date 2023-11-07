96dea16d2c5fcb70121aa46139b2749dff4bc6a2
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-07 16:52:15 +0800

    Update game_stats.py

--- Alien Invasion/game_stats.py
+++ Alien Invasion/game_stats.py
@@ -1,3 +1,6 @@
+import json
+
+
 class GameStats():
 	def __init__(self, ai_settings):
 		self.ai_settings = ai_settings
@@ -5,7 +8,16 @@ class GameStats():
 		self.game_active = False
 		self.score = 0
 		self.level = 1
-		self.high_score = 0
+		with open("high score data.json", 'r') as high_score_data_file:
+			high_score_data = json.load(high_score_data_file)
+		if 'high score' in high_score_data:
+			self.high_score = high_score_data['high score']
+		else:
+			self.high_score = 0
+		if 'user' in high_score_data:
+			self.high_score_user = high_score_data['user']
+		else:
+			self.high_score_user = None
 	
 	def reset_stats(self):
 		self.ships_left = self.ai_settings.ship_limit
