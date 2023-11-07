eaacd3990a671324d99a2db5116dd46da0ded194
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-07 16:30:48 +0800

    Update scoreboard.py

--- Alien Invasion/scoreboard.py
+++ Alien Invasion/scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 
 class ScoreBoard():
@@ -14,6 +17,7 @@ class ScoreBoard():
 		self.prep_score()
 		self.prep_high_score()
 		self.prep_level()
+		self.prep_ships()
 	
 	def prep_score(self):
 		round_score = int(round(self.stats.score, -1))
@@ -27,6 +31,8 @@ class ScoreBoard():
 		self.screen.blit(self.score_image, self.score_rect)
 		self.screen.blit(self.high_score_image, self.high_score_rect)
 		self.screen.blit(self.level_image, self.level_rect)
+		
+		self.ships.draw(self.screen)
 	
 	def prep_high_score(self):
 		high_score = int(round(self.stats.high_score, -1))
@@ -43,4 +49,12 @@ class ScoreBoard():
 		self.level_rect = self.level_image.get_rect()
 		self.level_rect.right = self.screen_rect.right - 20
 		self.level_rect.top = self.score_rect.bottom + 10
+	
+	def prep_ships(self):
+		self.ships = Group()
+		for ship_number in range(self.stats.ships_left):
+			ship = Ship(self.ai_settings, self.screen)
+			ship.rect.x = 10 + ship_number * ship.rect.width
+			ship.rect.y = 10
+			self.ships.add(ship)
 		
