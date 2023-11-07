fa4a6e2cae5b0a94bcc78d2594e29d086cf50e3e
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-07 17:17:58 +0800

    Update game_stats.py

--- Alien Invasion/game_stats.py
+++ Alien Invasion/game_stats.py
@@ -5,6 +5,7 @@ class GameStats():
 	def __init__(self, ai_settings):
 		self.ai_settings = ai_settings
 		self.reset_stats()
+		self.input_username = False
 		self.game_active = False
 		self.score = 0
 		self.level = 1
