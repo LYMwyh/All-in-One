bd1d07a4135e53747f88aa6487e85c814020cc48
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-07 22:35:55 +0800

    Update debug.py

--- Alien Invasion/debug.py
+++ Alien Invasion/debug.py
@@ -1,16 +1,56 @@
 import pygame
-import sys
+pygame.init()
 
-screen = pygame.display.set_mode((600, 1000))
 
-rect = pygame.Rect(10, 10, 100, 100)
+class InputBox:
 
-rect2 = pygame.Rect(11, 11, 99, 99)
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = pygame.Color('lightskyblue3')
+        self.text = text
+        self.txt_surface = pygame.font.Font(None, 32).render(text, True, self.color)
+        self.active = False
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-	screen.fill((60, 60, 60), rect)
-	screen.fill((230, 230, 230), rect2)
-	pygame.display.flip()
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = pygame.Color('dodgerblue2') if self.active else pygame.Color('lightskyblue3')
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = pygame.font.Font(None, 32).render(self.text, True, self.color)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+    def update(self):
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+input_box1 = InputBox(100, 100, 140, 32)
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        input_box1.handle_event(event)
+
+    screen.fill((30, 30, 30))
+    input_box1.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(30)
