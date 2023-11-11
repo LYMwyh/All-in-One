e35695fb136e090f06aeaededf8ece94cb22220c
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-11 13:04:44 +0800

    Update preprocessing.py

--- Alien Invasion/preprocessing.py
+++ Alien Invasion/preprocessing.py
@@ -16,19 +16,34 @@ def play_button_prep(play_button):
 def introduction_of_game_prep(ai_settings, screen, introduction_of_game):
 	introduction_of_game.append(
 		"This game called Alien Invasion.\n"
-		"You need to fire at the UFOs "
-		"and try not to let them reach the bottom line of the screen or touch you.\n"
+		"You need to fire at the UFOs \n "
+		"and try not to let them reach \n"
+		"the bottom line of the screen \n"
+		"or touch you.\n"
 		"If this happens, you will die.\n"
 		"You will have 3 lives each time.")
 	introduction_of_game.append(
-		"The first number in the upper left corner of the screen indicates you highest score.\n"
-		"The second number represents the score you got this time.\n"
-		"The third number represents your level. Every time you kill a UFO swarm, your level increases by one.\n"
+		"The first number in the upper \n"
+		"left corner of the screen indicates \n"
+		" you highest score.\n"
+		"The second number represents \n"
+		" the score you got this time.\n"
+		"The third number represents  \n"
+		"your level. Every time you kill \n"
+		"a UFO swarm, your level increases \n"
+		"by one.\n"
 		"Finally is your username.")
 	introduction_of_game.append(
-		"There is a number in the top center of the screen that represents the highest score among all users.\n"
-		"If there is a highest score, on the second line is the name of the user who achieved that score.\n"
-		"If there is no high score, the high score will be displayed as 0 and the second line will not appear.")
+		"There is a number in the top \n"
+		"center of the screen that  \n"
+		"represents the highest score \n"
+		"among all users.\n"
+		"If there is a highest score, \n"
+		"on the second line is the name \n"
+		"of the user who achieved that score.\n"
+		"If there is no high score, the \n"
+		"high score will be displayed as \n"
+		"0 and the second line will not appear.")
 	for page_ordinal, page in enumerate(copy.copy(introduction_of_game)):
 		introduction_of_game[page_ordinal] = Label(ai_settings, screen, introduction_of_game[page_ordinal])
 
