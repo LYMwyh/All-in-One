555786ba1a05ea6791665f32a34a8857e4903c03
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-11 19:49:05 +0800

    Update main.py

--- Alien Invasion/main.py
+++ Alien Invasion/main.py
@@ -16,7 +16,7 @@ def run_game():
 	pygame.init()
 	ai_settings = Settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion-1")
+	pygame.display.set_caption("Alien Invasion")
 	
 	stats = GameStats(ai_settings)
 	
