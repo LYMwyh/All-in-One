ffea293c24db0f7ee6c73161ae3fdd8114e6aa7d
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-15 00:18:10 +0800

    Update debug.py

--- twenty four/twenty four python tkinter/debug.py
+++ twenty four/twenty four python tkinter/debug.py
@@ -1,1 +1,6 @@
 # None
+import algorithm
+
+answer = ['(', algorithm.Fraction(1, 1), '/', algorithm.Fraction(1, 1), ')', '/', '(', algorithm.Fraction(1, 1), '/', algorithm.Fraction(1, 1), ')']
+print(algorithm.simplify_formula_first_part(answer))
+
