5fcd50a605ba2702ded041ef62a4cf2d9fd0257b
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-11-25 22:06:28 +0800

    Update debug.py

--- twenty four/twenty four python tkinter/debug.py
+++ twenty four/twenty four python tkinter/debug.py
@@ -1,7 +1,6 @@
 # None
 import algorithm
-# (7-3)*(1+5)
 
-answer = [algorithm.Fraction(1, 1), '-', algorithm.Fraction(1, 1),  '*', '(', algorithm.Fraction(1, 1), '-', algorithm.Fraction(24, 1), ')']
+answer = [1, '-', 1,  '*', '(', 1, '-', 24, ')']
 print(algorithm.simplify_formula_first_part(answer))
 
