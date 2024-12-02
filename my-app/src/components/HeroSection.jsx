c7ac06ab6aaabbe65bcf7b90ff48e6b02f7ab1f8
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-12-02 18:54:03 +0800

    restart project and set up SolidStart framework

--- my-app/src/components/HeroSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function HeroSection() {
-    return (
-        <div class="flex px-8 py-4 bg-slate-200 min-h-80">
-            <h1 class="text-6xl font-bold m-auto">I am <span class="inline-block text-7xl text-transparent bg-gradient-to-r from-blue-400 to-slate-600 bg-clip-text hover:scale-110 transition-transform">HelloWorld-er</span></h1>
-        </div>
-    )
-}
